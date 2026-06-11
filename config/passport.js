7ad229bf5484888d3b4a07f91faa324f62463bc7
brenker <brenker@theworkshop.com>
2026-06-11 15:39:46 +0200

    protect the create message route

--- config/passport.js
+++ config/passport.js
@@ -38,7 +38,7 @@ function isAuth(req, res, next) {
   if (req.isAuthenticated()) {
     next();
   } else {
-    res.send("loginForm");
+    res.status(404).json({'status': 'Unauthorized'});
   }
 }
 
--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -1,11 +1,11 @@
 import { Router } from "express"
 import { getAllMessages, createMessage } from "../controllers/messagesController.js"
-// import { isAuth } from '../config/passport'
+import { isAuth } from '../config/passport.js'
 
 const messagesRouter = Router();
 
 messagesRouter.get("/", getAllMessages)
-messagesRouter.post("/create", createMessage)
+messagesRouter.post("/create", isAuth, createMessage)
 // messagesRouter.post("/delete/:id", isAuth, deleteMessage)
 
 export default messagesRouter
