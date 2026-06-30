acb147d6489661d264a7830d7f2aace2ed50e795
brenker <brenker@theworkshop.com>
2026-06-30 18:20:14 +0200

    Accept second origin

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cors from "cors"
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'http://localhost:4001'],
   credentials: true
 }))         
 app.use(express.json());
